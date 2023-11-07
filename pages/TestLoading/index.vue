eb837adb457a5214c15bb0268b2fc977eaeee25c
trungdong11 <trungdong0811@gmail.com>
2023-11-07 09:58:12 +0700

    First Commit

--- pages/TestLoading/index.vue
+++ pages/TestLoading/index.vue
@@ -1,34 +1,57 @@
 <template>
-    <div>
-      <button @click="getResponseHTTP">Gửi yêu cầu HTTP</button>
-      <!-- <div>{{ listHttp }}</div> -->
-      <ul> 
-        <li v-for="item in data" :key="item.id">{{item.id}}</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        data: [],
-        url: "103.195.236.45",
-        iterations: 2,
-        threads: 50,
-      };
-    },
-    methods: {
-      async getResponseHTTP() {
-        try {
-          const response = await this.$api.httpProtocol.getHttpRequest(this.url, this.iterations, this.threads);
-          this.listHttp = response.data;
-          console.log('Done');
-        } catch (error) {
-          console.error(error);
-        }
+  <div>
+    <button @click="getResponseHTTP">Gửi yêu cầu HTTP</button>
+    <!-- <div>{{ listHttp }}</div> -->
+    <ul> 
+      <li v-for="listHttp in listHttps" :key="listHttp.id">{{listHttp}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // data: [],
+      listHttps: [],
+      url: "103.195.236.45",
+      iterations: 2,
+      threads: 2,
+    };
+  },
+  created() {
+    this.getResponseHTTP();
+  },
+  methods: {
+    async getResponseHTTP() {
+      try {
+        const response = await this.$api.httpProtocol.getHttpRequest(this.url, this.iterations, this.threads);
+        const sseUrl = response.data;
+        let result = this.setupEventSource(sseUrl);
+        console.log(result);
+      } catch (error) {
+        console.error(error);
       }
     },
-  };
-  </script>
-  
+    setupEventSource(sseUrl) {
+      // const source = new EventSource(sseUrl);
+      // console.log(source)
+
+      // source.addEventListener('message', event => {
+      //     let data = JSON.parse(event.data);
+      //     this.listHttps = data.listHttps;
+      // }, false);
+
+      // source.addEventListener('error', event => {
+      //     if (event.readyState == EventSource.CLOSED) {
+      //         console.log('Event was closed');
+      //         console.log(EventSource);
+      //     }
+      // }, false);
+    }
+}
+}
+</script>
+<style scoped>
+
+</style>
