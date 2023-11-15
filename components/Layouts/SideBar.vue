2a74d3db1089dce3cc7ca7b768a272004c5fe409
trungdong11 <trungdong0811@gmail.com>
2023-11-15 21:43:15 +0700

    update

--- components/Layouts/SideBar.vue
+++ components/Layouts/SideBar.vue
@@ -5,55 +5,26 @@
                 <div class="sidebar-header">
                     <!-- <img class="header-logo" src="~/assets/img/Logo-sgo.png" alt=""> -->
                     <div class="collapse-btn" @click="collapseSidebar()">
-                        
-                        <!-- <img src="~/assets/svg/collapseLeft.svg" alt="" class="collapse-btn-icon-left"> -->
-                        <!-- <img src="~/assets/svg/collapseRight.svg" alt="" class="collapse-btn-icon-right"> -->
+                    
                     </div>
                 </div>
-                <p class="dashboard-title">DASHBOARD</p>
+                <p class="dashboard-title">FEATURE</p>
                 <div class="sidebar-list">
-                    <!-- <nuxt-link to="/" class="sidebar-item" :class="{ 'active': pageParams == '/' }">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-                            <path fill="currentColor"
-                                d="M4 19v-7.375L3 12.4q-.35.25-.75.2t-.65-.4q-.25-.35-.188-.75t.388-.65l8.975-6.875q.275-.2.588-.3t.637-.1q.325 0 .638.1t.587.3l9 6.875q.325.25.375.65t-.2.75q-.25.325-.65.375t-.725-.2L20 11.625V19q0 .825-.587 1.413T18 21H6q-.825 0-1.413-.588T4 19Zm2 0h12v-8.9l-6-4.575L6 10.1V19Zm2-4q.425 0 .713-.288T9 14q0-.425-.288-.713T8 13q-.425 0-.713.288T7 14q0 .425.288.713T8 15Zm4 0q.425 0 .713-.288T13 14q0-.425-.288-.713T12 13q-.425 0-.713.288T11 14q0 .425.288.713T12 15Zm4 0q.425 0 .713-.288T17 14q0-.425-.288-.713T16 13q-.425 0-.713.288T15 14q0 .425.288.713T16 15ZM6 19h12H6Z" />
-                        </svg>
-                        Dashboard
-                    </nuxt-link> -->
-                     <nuxt-link to="/TestLoading" class="sidebar-item" :class="{ 'active': pageParams.startsWith('/TestLoading') }">
+                    <nuxt-link to="/TestLoading" class="sidebar-item" :class="{ 'active': pageParams.startsWith('/TestLoading') }">
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
                             <path fill="currentColor"
                                 d="M10.275 12q-.7 0-1.15-.525T8.8 10.25l.3-1.8q.2-1.075 1.013-1.763T12 6q1.1 0 1.913.688t1.012 1.762l.3 1.8q.125.7-.325 1.225T13.75 12h-3.475Zm-7.175.975q-.575.025-.988-.225t-.537-.775q-.05-.225-.025-.45t.125-.425q0 .025-.025-.1q-.05-.05-.25-.6q-.05-.3.075-.575T1.8 9.35l.05-.05q.05-.475.388-.8t.837-.325q.075 0 .475.1l.075-.025q.125-.125.325-.187T4.375 8q.275 0 .488.088t.337.262q.025 0 .038.013t.037.012q.35.025.612.212t.388.513q.05.175.038.338t-.063.312q0 .025.025.1q.175.175.275.388t.1.437q0 .1-.15.525q-.025.05 0 .1l.05.4q0 .525-.438.9t-1.062.375H3.1ZM20 13q-.825 0-1.413-.588T18 11q0-.3.088-.563t.237-.512l-.7-.625q-.25-.2-.088-.5T18 8.5h2q.825 0 1.413.588T22 10.5v.5q0 .825-.588 1.413T20 13ZM0 18v-1.575q0-1.1 1.113-1.763T4 14q.325 0 .625.013t.575.062q-.35.5-.525 1.075T4.5 16.375V18H0Zm6 0v-1.625q0-1.625 1.663-2.625t4.337-1q2.7 0 4.35 1T18 16.375V18H6Zm14-4q1.8 0 2.9.663t1.1 1.762V18h-4.5v-1.625q0-.65-.163-1.225t-.487-1.075q.275-.05.563-.062T20 14Z" />
                           </svg>
                         TestLoading
                     </nuxt-link>
-                    <!--
-                    <nuxt-link to="/customers" class="sidebar-item" :class="{ 'active': pageParams.startsWith('/customers') }">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm3.61 6.34c1.07 0 1.93.86 1.93 1.93s-.86 1.93-1.93 1.93s-1.93-.86-1.93-1.93c-.01-1.07.86-1.93 1.93-1.93zm-6-1.58c1.3 0 2.36 1.06 2.36 2.36s-1.06 2.36-2.36 2.36s-2.36-1.06-2.36-2.36c0-1.31 1.05-2.36 2.36-2.36zm0 9.13v3.75c-2.4-.75-4.3-2.6-5.14-4.96c1.05-1.12 3.67-1.69 5.14-1.69c.53 0 1.2.08 1.9.22c-1.64.87-1.9 2.02-1.9 2.68zM12 20c-.27 0-.53-.01-.79-.04v-4.07c0-1.42 2.94-2.13 4.4-2.13c1.07 0 2.92.39 3.84 1.15C18.28 17.88 15.39 20 12 20z"/>
-                        </svg>
-                        Customers
-                    </nuxt-link>
-                    <nuxt-link to="/bookings" class="sidebar-item" :class="{ 'active': pageParams.startsWith('/bookings') }">
+                    <nuxt-link to="/TestJDBC" class="sidebar-item" :class="{ 'active': pageParams.startsWith('/TestJDBC') }">
                         <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
                             <path fill="currentColor"
-                                d="m17.275 20.25l3.475-3.45l-1.05-1.05l-2.425 2.375l-.975-.975l-1.05 1.075l2.025 2.025ZM6 9h12V7H6v2Zm12 14q-2.075 0-3.538-1.463T13 18q0-2.075 1.463-3.538T18 13q2.075 0 3.538 1.463T23 18q0 2.075-1.463 3.538T18 23ZM3 22V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v6.675q-.475-.225-.975-.375T19 11.075V5H5v14.05h6.075q.125.775.388 1.475t.687 1.325L12 22l-1.5-1.5L9 22l-1.5-1.5L6 22l-1.5-1.5L3 22Zm3-5h5.075q.075-.525.225-1.025t.375-.975H6v2Zm0-4h7.1q.95-.925 2.213-1.463T18 11H6v2Zm-1 6.05V5v14.05Z" />
-                        </svg>
-                        Bookings
+                                d="M10.275 12q-.7 0-1.15-.525T8.8 10.25l.3-1.8q.2-1.075 1.013-1.763T12 6q1.1 0 1.913.688t1.012 1.762l.3 1.8q.125.7-.325 1.225T13.75 12h-3.475Zm-7.175.975q-.575.025-.988-.225t-.537-.775q-.05-.225-.025-.45t.125-.425q0 .025-.025-.1q-.05-.05-.25-.6q-.05-.3.075-.575T1.8 9.35l.05-.05q.05-.475.388-.8t.837-.325q.075 0 .475.1l.075-.025q.125-.125.325-.187T4.375 8q.275 0 .488.088t.337.262q.025 0 .038.013t.037.012q.35.025.612.212t.388.513q.05.175.038.338t-.063.312q0 .025.025.1q.175.175.275.388t.1.437q0 .1-.15.525q-.025.05 0 .1l.05.4q0 .525-.438.9t-1.062.375H3.1ZM20 13q-.825 0-1.413-.588T18 11q0-.3.088-.563t.237-.512l-.7-.625q-.25-.2-.088-.5T18 8.5h2q.825 0 1.413.588T22 10.5v.5q0 .825-.588 1.413T20 13ZM0 18v-1.575q0-1.1 1.113-1.763T4 14q.325 0 .625.013t.575.062q-.35.5-.525 1.075T4.5 16.375V18H0Zm6 0v-1.625q0-1.625 1.663-2.625t4.337-1q2.7 0 4.35 1T18 16.375V18H6Zm14-4q1.8 0 2.9.663t1.1 1.762V18h-4.5v-1.625q0-.65-.163-1.225t-.487-1.075q.275-.05.563-.062T20 14Z" />
+                          </svg>
+                        TestJDBC
                     </nuxt-link>
-                    <nuxt-link to="/userInfors" class="sidebar-item" :class="{ 'active': pageParams.startsWith('/userInfors') }">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="currentColor" d="m9.83 18l.59.59L12 20.17l1.59-1.59l.58-.58H19V4H5v14h4.83zM12 5c1.65 0 3 1.35 3 3s-1.35 3-3 3s-3-1.35-3-3s1.35-3 3-3zM6 15.58C6 13.08 9.97 12 12 12s6 1.08 6 3.58V17H6v-1.42z" opacity=".3"/>
-                            <path fill="currentColor" d="m9 20l3 3l3-3h4c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h4zM5 4h14v14h-4.83l-.59.59L12 20.17l-1.59-1.59l-.58-.58H5V4zm7 7c1.65 0 3-1.35 3-3s-1.35-3-3-3s-3 1.35-3 3s1.35 3 3 3zm0-4c.55 0 1 .45 1 1s-.45 1-1 1s-1-.45-1-1s.45-1 1-1zm6 8.58c0-2.5-3.97-3.58-6-3.58s-6 1.08-6 3.58V17h12v-1.42zM8.48 15c.74-.51 2.23-1 3.52-1s2.78.49 3.52 1H8.48z"/>
-                        </svg>
-                        UserInfors
-                        <div v-if="countRequest?.count != 0" class="count">
-                            <p>{{ countRequest?.count }}</p>
-                        </div>
-                    </nuxt-link>
-                    <nuxt-link to="/calendars" class="sidebar-item" :class="{ 'active': pageParams.startsWith('/calendars') }">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
-                            <path fill="currentColor" d="M8 14q-.425 0-.713-.288T7 13q0-.425.288-.713T8 12q.425 0 .713.288T9 13q0 .425-.288.713T8 14Zm4 0q-.425 0-.713-.288T11 13q0-.425.288-.713T12 12q.425 0 .713.288T13 13q0 .425-.288.713T12 14Zm4 0q-.425 0-.713-.288T15 13q0-.425.288-.713T16 12q.425 0 .713.288T17 13q0 .425-.288.713T16 14ZM5 22q-.825 0-1.413-.588T3 20V6q0-.825.588-1.413T5 4h1V2h2v2h8V2h2v2h1q.825 0 1.413.588T21 6v14q0 .825-.588 1.413T19 22H5Zm0-2h14V10H5v10Z"/>
-                        </svg>
-                        Calendars
-                    </nuxt-link> -->
+                   
                 </div>
 
             </div>
@@ -111,16 +82,16 @@ export default {
     
 }
 .sidebar-element {
-    width: 280px;
+    width: 180px;
 }
 
 .sidebar-main {
-    width: 280px;
+    width: 180px;
     position: fixed;
     height: 100vh;
     top: 0;
     left: 0;
-    background: #ffffff;
+    background: #D1FAEC;
     padding-top: 16px;
     transition: width 225ms cubic-bezier(0.4, 0, 0.6, 1) 0ms;
     overflow-x: hidden;
@@ -174,7 +145,7 @@ export default {
     gap: 15px;
     padding: 10px 24px;
     align-items: center;
-    width: 280px;
+    width: 180px;
     color: #626C70;
     text-decoration: none;
     border-left: 3px solid transparent;
--- pages/TestLoading/index.vue
+++ pages/TestLoading/index.vue
@@ -1,39 +1,50 @@
 <template>
   <div>
-    <!-- <div class="container">
-      <form action="" @submit.prevent="getResponseHTTP">
-        <label for="">URL or Ip address</label>
-        <input type="text" placeholder="Enter the url" v-model="urlValue">
+      <div class="container">
+          <div class="container__item btn ">
+              <input v-model="urlValue" type="text" placeholder="Enter the url" />
+              <button @click="getResponseHTTP">Start test</button>
+          </div>
 
-        <label for="">Thread quantity</label>
-        <input type="text" placeholder="Enter the thread quantity" v-model="threadValue">
+          <div class="container__item">
+            <!-- <label for="">Thread quantity</label> -->
+            <input v-model="threadsValue" type="text" placeholder="Enter the thread quantity" />
+            <input v-model="iterationValue" type="text" placeholder="Enter the iteration" />  
+          </div>
 
-        <label for="">Iteration quantity</label>
-        <input type="text" placeholder="Enter the iteration" v-model="IterationValue">
+          <div class="container__result" v-if="isCheck == true">
+            <div class="container__result__header">
+              <h2>Discover what your real users are experiencing</h2>
+            </div>
+            <div class="container__result__content">
+              <div class="col-2">
+                <ul> 
+                  <li v-for="listHttp in listHttps" :key="listHttp.id">{{listHttp}}</li>
+                </ul> 
+              </div>
+              <!-- <div class="col-2">
 
-        <button type="submit">Start test</button>
-      </form>
-    </div> -->
-    <button @click="getResponseHTTP">Gửi yêu cầu HTTP</button>
-    <!-- <div>{{ listHttp }}</div> -->  
-    <p> 
-      <!-- <li v-for="listHttp in listHttps" :key="listHttp.id">{{listHttp}}</li> -->
-      {{ listHttps }}
-    </p> 
-  </div>
+              </div> -->
+            </div>
+          </div>
+          
+          
+      </div>
+
+  </div>  
 </template>
 
 <script>
 export default {
   data() {
     return {
-      // data: [],
-      listHttps: [],
-      urlValue: '',
-      iterationValue: '',
-      threadsValue: '',
+      listHttps: [],  
+      urlValue: "",
+      iterationValue: "",
+      threadsValue: "",
+      isCheck: false,
        
-    };
+    }
   },
   created() {
     // this.getResponseHTTP();
@@ -41,7 +52,7 @@ export default {
   methods: {
     async getResponseHTTP() {
       this.$axios({
-        url: 'http://localhost:8081/api/v1/http-methods/get/http/103.195.237.70?threads=2&iterations=2',
+        url: `http://localhost:8081/api/v1/http-methods/get/http/${this.urlValue}?threads=${this.threadsValue}&iterations=${this.iterationValue}`,
         data: {
           prompt: 'json data'
         },
@@ -52,18 +63,13 @@ export default {
         method: 'GET',
         onDownloadProgress: progressEvent => {
           const xhr = progressEvent?.target
-          // console.log(xhr.response.data)
-          this.listHttps = xhr.response
-          const dataString = xhr.response.split('data:') 
-          console.log(dataString)
-          // const result = JSON.parse(dataString)
-          
-          // console.log(dataString)
-
-          // const{responseText} = xhr;
-          // const dataString = responseText .split(' ');
-          // this.listHttps = dataString
-          // console.log(dataString)
+          const dataString = xhr.responseText.replace(/data:/g, '');
+          const lines = dataString.split('\n');
+          const filteredArray = lines.filter(obj => Object.keys(obj).length > 0);
+          const Arr = filteredArray.map(line => JSON.parse(line));
+          console.log(Arr)
+          this.listHttps = Arr;
+          this.isCheck = true;
         }
       }).then(({ data }) => Promise.resolve(data));
     },
@@ -73,5 +79,84 @@ export default {
 </script>
 <style scoped lang="scss">
 
+.container {
+  margin: 28px 38px;
+
+  &__item {
+    
+    display: flex;
+    column-gap: 12px;
+    width: 88%;
+
+    input {
+      width: 100%;
+      height: 48px;
+      border-radius: 10px;
+      border: #0066ff 1px solid;
+      outline: none;
+      padding: 0 20px;
+      font-size: 16px;
+      margin-bottom: 24px;
+    }
+
+    button {
+      height: 48px;
+      width: 12%;
+      margin-left: 8px;
+      font-size: 16px;
+      font-weight: 600;
+      color: #ffff;
+      background-color: #0066ff;
+      border: none;
+      outline: none;
+      border-radius: 10px;
+      padding: 0 12px;
+      cursor: pointer;
+    }
+
+    button:hover {
+      opacity: 0.8;
+    }
+  }
+
+  &__item.btn {
+    display: flex;
+    column-gap: 12px;
+    width: 100%;
+  }
+
+  &__result {
+    
+    &__header {
+      font-size: 28px;
+      font-weight: 600;
+      text-align: center;
+      margin-top: 24px;
+      margin-bottom: 24px;
+    }
+
+    &__content {
+      display: flex;
+      border: 1px solid #000;
+      border-radius: 6px;
+
+      .col-2 {
+        column-gap: 24px;
+        //width: 50%;
+        padding: 16px;
+        border-right: 1px solid #000;
+        
+        ul {
+          list-style: none;
+
+          li {
+            padding: 6px 0;
+            border-bottom: 1px solid #9999;
+          }
+        }
+      }
+    }
+  }
+}
 
 </style>
